d41251bce5fffcb24224952e6e7bfa4478ce0d23
marcelrm11 <marcelrm11@gmail.com>
2022-12-03 10:31:28 +0000

    bootstrap-react installed

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from 'react'
 import {createRoot} from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,12 +3,9 @@ import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>Hello Marcel!</h1>
 		<p>
 			<img src="https://picsum.photos/200" />
 		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
 	</div>
 );
