30fac18894b66cfa60f3261f0e4fc1baa4c4293e
marcelrm11 <marcelrm11@gmail.com>
2022-12-06 19:00:28 +0000

    load API refactoring

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -6,72 +6,25 @@ const getState = ({ getStore, getActions, setStore }) => {
       },
     },
     actions: {
-      // Use getActions to call a function within a fuction
-      loadPeople: () => {
+      loadData: (group) => {
         const store = getStore();
         const actions = getActions();
-        // console.log("loading people");
-        fetch("https://www.swapi.tech/api/people")
+        fetch(`https://www.swapi.tech/api/${group}`)
           .then((response) => response.json())
           .then((resdata) => {
-            const newData = { ...store.data, people: resdata };
+            const newData = { ...store.data, [group]: resdata };
             setStore({ data: newData });
-            // console.log("people loaded");
             return newData;
           })
           .then((data) => {
-            // refactor with function (repeats in planets and vehicles)
-            let results = data.people.results;
+            let results = data[group].results;
             for (let i = 0; i < results.length; i++) {
-              // actions.loadDetail("people", results[i].uid);
-              actions.loadDetail("people", results[i].uid);
+              actions.loadDetail(group, results[i].uid);
             }
           })
           .catch((error) => console.log(error));
-      },
-      loadPlanets: () => {
-        // console.log("loading planets");
-        const store = getStore();
-        const actions = getActions();
-        fetch("https://www.swapi.tech/api/planets")
-          .then((response) => response.json())
-          .then((resdata) => {
-            const newData = { ...store.data, planets: resdata };
-            setStore({ data: newData });
-            // console.log("planets loaded");
-            return newData;
-          })
-          .then((data) => {
-            let results = data.planets.results;
-            for (let i = 0; i < results.length; i++) {
-              actions.loadDetail("planets", results[i].uid);
-            }
-          })
-          .catch((error) => console.log(error));
-      },
-      loadVehicles: () => {
-        const store = getStore();
-        const actions = getActions();
-        // console.log("loading vehicles");
-        fetch("https://www.swapi.tech/api/vehicles")
-          .then((response) => response.json())
-          .then((resdata) => {
-            const newData = { ...store.data, vehicles: resdata };
-            setStore({ data: newData });
-            // console.log("vehicles loaded");
-            return newData;
-          })
-          .then((data) => {
-            let results = data.vehicles.results;
-            for (let i = 0; i < results.length; i++) {
-              actions.loadDetail("vehicles", results[i].uid);
-            }
-          })
-          .catch((error) => console.log(error))
-          .finally(() => console.log(store));
       },
       loadDetail: (group, id) => {
-        // console.log("loading detail");
         fetch(`https://www.swapi.tech/api/${group}/${id}`)
           .then((response) => response.json())
           .then((resdata) => resdata.result.properties)
@@ -94,7 +47,6 @@ const getState = ({ getStore, getActions, setStore }) => {
                 }
               }
               setStore({ data: updatedStore.data });
-              // console.log(`detail loaded: ${group}/${id}`);
             }
           })
           .catch((error) => console.log(error));
